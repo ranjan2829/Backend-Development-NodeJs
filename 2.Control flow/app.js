9f415cbfedae0b54bee70a43e79e37e0dcb078d4
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-10-30 15:12:56 +0530

    basics

--- 2.Control flow/app.js
+++ 2.Control flow/app.js
@@ -1,11 +1,25 @@
-let a=20;
-let b=20;
-if(a <  b){
-    console.log("b is smaller than a")
-}
-else if (a===b){
-    console.log("equal !!!")
-}
-else{
-    console.log("Trueee!!!!")
+// let a=20;
+// let b=20;
+// if(a <  b){
+//     console.log("b is smaller than a")
+// }
+// else if (a===b){
+//     console.log("equal !!!")
+// }
+// else{
+//     console.log("Trueee!!!!")
+// }
+let time = 50;
+let greetings;
+if (time < 10) {
+    greetings = "Good Morning";
+    console.log(greetings);
+} 
+else if (time < 20) {
+    greetings = "Good Day"; 1
+    console.log(greetings);
+} 
+else {
+greetings = "Good Evening";
+console.log(greetings);
 }
