48052540854304503c9accd1ce6c31b102c3c9f7
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-10-30 15:10:49 +0530

    basics

--- 2.Control flow/app.js
+++ 2.Control flow/app.js
@@ -1,8 +1,11 @@
-let a=100;
+let a=20;
 let b=20;
 if(a <  b){
     console.log("b is smaller than a")
 }
+else if (a===b){
+    console.log("equal !!!")
+}
 else{
-    console.log("True!!!")
+    console.log("Trueee!!!!")
 }
