98911171255315fa1534964ff85d3a6fbab3311d
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-10-31 14:54:16 +0530

    basics

--- DOM/app.js
+++ DOM/app.js
@@ -2,3 +2,6 @@ console.log(document.head);
 console.log(document.getElementsByTagName("h1"))
 
 console.log(document.getElementById("main"))
+
+console.log(document.getElementsByClassName(".cls"))
+console.log(document.querySelectorAll("h1"));
---
+++ innerHTml/app.js
@@ -0,0 +1,7 @@
+const p=document.querySelector("p");
+console.log(p.innerText)
+console.log(p.textContent)
+console.log(p.innerHTML)
+
+const h1=document.querySelector("h1");
+//h1.innerText="Ranjan S";
