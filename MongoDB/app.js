c2f7c4c5dcfbfc144a20c584727bbf7328fdb8b2
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-11-24 13:43:42 +0530

    mongoDB BAckend

---
+++ MongoDB/app.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const uri = "mongodb+srv://ranjanshitole3129:ranjan3129@cluster0.qpnmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+mongoose.connect(uri)
+  .then(() => {
+    console.log("Successfully connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+
+const userDb = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number,  
+    isActive: Boolean,
+    tags: [String],
+    createdAt: { type: Date, default: Date.now }
+});
+
+
+const User = mongoose.model("User", userDb);
+
+async function AddRecord() {
+    try {
+        const newUser = await User.create({
+            name: "Ranjan",
+            email: "Ranjan.shitole3129@gmail.com",
+            age: 21,  
+            isActive: true,
+            tags: ["developer"]
+        });
+
+        console.log("New user created: ", newUser); 
+
+    } catch (err) {
+        console.error("Error creating user: ", err); 
+    }
+    finally{
+
+    
+     await mongoose.connection.close();
+    }
+    
+}
+
+
+
+AddRecord();
